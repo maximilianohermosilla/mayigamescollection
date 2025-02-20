caac9eef8d46312e50ac358cc50f1b0979d70dea
maximiliano_hermosilla <maximiliano_hermosilla@hotmail.com>
2025-02-20 00:35:29 -0300

    Renderización de cards de juegos paginadas con plataformas y dos modos de vista. Filtro funcionando, falta ordenamiento

--- src/app/features/pages/juegos/juegos.component.ts
+++ src/app/features/pages/juegos/juegos.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { JuegosService } from 'src/app/core/services/juegos.service';
 import { SpinnerService } from 'src/app/core/services/spinner.service';
+import { SelectItem } from 'primeng/api';
+import { DataView } from 'primeng/dataview';
 
 @Component({
     templateUrl: './juegos.component.html',
@@ -11,6 +13,14 @@ export class JuegosComponent implements OnInit{
     public spinnerService = inject(SpinnerService);
 
     public listaJuegos: any[] = [];
+    public defaultImage: string = "/assets/placeholder.jpg";
+
+    sortOptions: SelectItem[] = [];
+    sortOrder: number = 0;
+    sortField: string = '';
+    sourceCities: any[] = [];
+    targetCities: any[] = [];
+    orderCities: any[] = [];
 
     public cargarSpinner(){
         this.spinnerService.showSpinner();
@@ -25,5 +35,23 @@ export class JuegosComponent implements OnInit{
             this.listaJuegos = response;
             console.log(response);
         })
+    }
+    
+    onSortChange(event: any) {
+        const value = event.value;
+
+        if (value.indexOf('!') === 0) {
+            this.sortOrder = -1;
+            this.sortField = value.substring(1, value.length);
+        } else {
+            this.sortOrder = 1;
+            this.sortField = value;
+        }
+    }
+
+    onFilter(dv: DataView, event: Event) {
+        console.log(dv)
+        console.log(event)
+        dv.filter((event.target as HTMLInputElement).value);
     }
 }
--- src/app/features/pages/juegos/juegos.module.ts
+++ src/app/features/pages/juegos/juegos.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { JuegosRoutingModule } from './juegos-routing.module';
 import { JuegosComponent } from './juegos.component';
 import { ButtonModule } from 'primeng/button';
+import { DataViewModule } from 'primeng/dataview';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         ButtonModule,
+        DataViewModule,
+        DropdownModule,
+        InputTextModule,
         JuegosRoutingModule
     ],
     declarations: [JuegosComponent]
